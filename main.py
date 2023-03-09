e4f4508dc1dc0aff1e41925ca0c7eeb22576cb35
georgecwan <georgecwan@users.noreply.github.com>
2023-03-09 13:09:52 -0500

    Remove print statements from main.py

--- main.py
+++ main.py
@@ -59,11 +59,8 @@ class RedditBot:
                 'If-Modified-Since': self.db[key]["Last_Modified"],
                 'If-None-Match': self.db[key]["ETag"]
             })
-            print(resp.status_code)
             if resp.status_code == 200:
                 self.db[key]["Last_Modified"] = resp.headers['Last-Modified'] if 'Last-Modified' in resp.headers else None
-                print(self.db[key]["ETag"])
-                print(resp.headers['ETag'])
                 self.db[key]["ETag"] = resp.headers['ETag'] if 'ETag' in resp.headers else None
                 return resp.text
             else:
