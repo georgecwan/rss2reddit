751b6f0fbe3f830a491dec5330c526849781815f
georgecwan <georgecwan@users.noreply.github.com>
2023-11-16 12:29:45 -0800

    Refactor reddit_bot to reduce nesting

--- rss_script/reddit_bot.py
+++ rss_script/reddit_bot.py
@@ -63,7 +63,7 @@ class RedditBot:
         # Initializes db dictionary, sets self.db
         self.db_file = db_file
         self.db = utils.load_db(db_file)
-        self.initialize_db()
+        self._initialize_db()
         # Initializes reddit praw object
         self.reddit = praw.Reddit(username=self.credentials['user'], password=self.credentials['password'],
                                   client_id=self.credentials['client_id'],
@@ -73,7 +73,7 @@ class RedditBot:
         utils.send_discord_message(
             f"Bot Started on {'Testing Mode' if self.testing else 'Normal Mode'} at {time.strftime('%Y-%m-%d %H:%M')}")
 
-    def initialize_db(self) -> None:
+    def _initialize_db(self) -> None:
         """
         Initializes the database by adding any new subreddits and RSS feeds to the database
 
@@ -108,13 +108,89 @@ class RedditBot:
             print("The file might be corrupted, try deleting db.json then try again.")
             sys.exit(1)
 
-    def rss_request(self, url: str, subreddit: str) -> str | None:
+    def run(self) -> None:
+        """
+        Main program loop that checks the RSS feeds, updates the db.json file, then sleeps for a specified amount of
+        time.
+        """
+        while True:
+            print(f"\n[{time.strftime('%Y-%m-%d %H:%M')}] Checking RSS feeds...")
+            next_update = self._subreddits_loop()
+            # Update db.json
+            if not self.testing:
+                print("Updating db.json...")
+                utils.update_db(self.db_file, self.db)
+            # Wait until next update
+            t = datetime.fromtimestamp(next_update).strftime('%Y-%m-%d %H:%M')
+            print(f"Next update at {t}")
+            utils.until(next_update)
+
+    def _subreddits_loop(self) -> float:
+        """
+        Loops through all the subreddits and RSS feeds and checks for new entries
+
+        Returns:
+            The time in seconds until the next time the loop should run
+        """
+        next_update = math.inf  # next_update is the time in seconds until the next update
+        for sub_info in self.sub_list:
+            print(f"Checking r/{sub_info['name']}...")
+            updates = self.db[sub_info['name']]['update_list']
+            sources = self.db[sub_info['name']]['rss_sources']
+            for index in range(len(sub_info['rss_feeds'])):
+                current_feed = sub_info['rss_feeds'][index]
+                update_entry = updates[index]
+                url = current_feed['urls'][update_entry['update_index']]
+                # Update time has passed
+                if update_entry['update_time'] <= math.ceil(time.time()):
+                    next_update = min(self._handle_update(sub_info, sources, current_feed, update_entry, url),
+                                      next_update)
+                # Update time has not passed
+                else:
+                    next_update = min(update_entry['update_time'], next_update)
+        return next_update
+
+    def _handle_update(self, sub_info: dict, sources: dict, current_feed: dict, update_entry: dict, url: str) -> float:
+        """
+        Updates a specific subreddit-RSS feed combination by making a request to the RSS feed, updating the db, and
+        posting to Reddit if applicable.
+
+        Args:
+            sub_info: Config dictionary for the subreddit
+            sources: DB entry containing list of URL and update about the RSS feeds to check for the subreddit
+            current_feed: DB entry containing a group of RSS feeds to check and the update interval
+            update_entry: DB entry containing the update time, update index, and listening mode for the RSS feed group
+            url: The url of the RSS feed
+
+        Returns:
+            Timestamp of when this subreddit-RSS feed combination should next enter listening mode.
+        """
+        # New Entry
+        if url not in sources:
+            sources[url] = {'last_modified': None, 'etag': None, 'last_id': None}
+            print(f"Adding {url} to db...")
+        # Get RSS Feed
+        response_text = self._rss_request(url, sub_info['name'])
+        if not response_text:
+            print(f"No new data from {url}, continuing to listen")
+            new_update = int(time.time()) + 1800  # Check 30 minutes later
+            update_entry.update({'update_time': new_update, 'listening': True})
+            return new_update
+        title, link, guid = utils.find_newest_headline(response_text)
+        # Check for errors from RSSParser.py
+        if not title:
+            return math.inf
+        return self._handle_rss_response(sub_info, sources, current_feed, update_entry, url, title,
+                                         link, guid)
+
+    def _rss_request(self, url: str, subreddit: str) -> str | None:
         """
         Makes a request to the RSS feed and returns the response text if the request is successful
 
         Args:
             url: The url of the RSS feed
             subreddit: The name of the subreddit with the RSS entry
+
         Returns:
             The response text of the RSS feed if the request is successful, None otherwise
         """
@@ -133,76 +209,57 @@ class RedditBot:
             print(f'Error requesting {url}: {str(e)}')
             return None
 
-    def subreddits_loop(self) -> int:
+    def _handle_rss_response(self, sub_info: dict, sources: dict, current_feed: dict, update_entry: dict, url: str,
+                             title: str, link: str, guid: str) -> int:
         """
-        Loops through all the subreddits and RSS feeds and checks for new entries
+        Handles the response from the RSS feed by updating the db, managing the listening mode, and posting to Reddit if
+        applicable.
+
+        Args:
+            sub_info: Config dictionary for the subreddit
+            sources: DB entry containing list of URL and update about the RSS feeds to check for the subreddit
+            current_feed: DB entry containing a group of RSS feeds to check and the update interval
+            update_entry: DB entry containing the update time, update index, and listening mode for the RSS feed group
+            url: The url of the RSS feed
+            title: Title of the latest article from the RSS feed
+            link: Link to the latest article from the RSS feed
+            guid: GUID of the latest article from the RSS feed
 
         Returns:
-            The time in seconds until the next time the loop should run
+
         """
-        next_update = math.inf  # next_update is the time in seconds until the next update
-        for sub_info in self.sub_list:
-            print(f"Checking r/{sub_info['name']}...")
-            updates = self.db[sub_info['name']]['update_list']
-            sources = self.db[sub_info['name']]['rss_sources']
-            for index in range(len(sub_info['rss_feeds'])):
-                current_feed = sub_info['rss_feeds'][index]
-                update_entry = updates[index]
-                url = current_feed['urls'][update_entry['update_index']]
-                # Update time has passed
-                if update_entry['update_time'] <= math.ceil(time.time()):
-                    # New Entry
-                    if url not in sources:
-                        sources[url] = {'last_modified': None, 'etag': None, 'last_id': None}
-                        print(f"Adding {url} to db...")
-                    # Get RSS Feed
-                    response_text = self.rss_request(url, sub_info['name'])
-                    if not response_text:
-                        print(f"No new data from {url}, continuing to listen")
-                        new_update = int(time.time()) + 1800  # Check 30 minutes later
-                        next_update = min(new_update, next_update)
-                        update_entry.update({'update_time': new_update, 'listening': True})
-                        continue
-                    title, link, guid = utils.find_newest_headline(response_text)
-                    # Check for errors from RSSParser.py
-                    if not title:
-                        continue
-                    # Update db and post to Reddit
-                    if not update_entry['listening']:
-                        print(f"Began listening")
-                        new_update = int(time.time()) + 1800  # Check 30 minutes later
-                        sources[url]['last_id'] = guid
-                        update_entry.update({'update_time': new_update, 'listening': True})
-                    elif sources[url]['last_id'] == guid:
-                        print(f"No new stories from {url}")
-                        new_update = int(time.time()) + 1800  # Check 30 minutes later
-                        update_entry.update({'update_time': new_update})
-                    else:
-                        print("New story found! Checking for duplicates...")
-                        if self.check_for_duplicates(title, link, self.reddit.subreddit(sub_info['name'])):
-                            new_update = int(time.time()) + 1800  # Check 30 minutes later
-                            sources[url]['last_id'] = guid
-                            update_entry.update({'update_time': new_update, 'listening': True})
-                        else:
-                            print("No duplicates found, posting to Reddit...")
-                            self.post_to_subreddit(sub_info['name'], title, link,
-                                                   current_feed['flair'] if 'flair' in current_feed else None)
-                            new_update = int(time.time()) + current_feed['check_interval']
-                            sources[url]['last_id'] = guid
-                            update_entry.update({'update_time': new_update,
-                                                 'update_index': (update_entry['update_index'] + 1) % len(
-                                                     current_feed['urls']), 'listening': False})
-                    next_update = min(new_update, next_update)
-                # Update time has not passed
-                else:
-                    next_update = min(update_entry['update_time'], next_update)
-        return next_update
+        # Update db and post to Reddit
+        if not update_entry['listening']:
+            print(f"Began listening")
+            new_update = int(time.time()) + 1800  # Check 30 minutes later
+            sources[url]['last_id'] = guid
+            update_entry.update({'update_time': new_update, 'listening': True})
+        elif sources[url]['last_id'] == guid:
+            print(f"No new stories from {url}")
+            new_update = int(time.time()) + 1800  # Check 30 minutes later
+            update_entry.update({'update_time': new_update})
+        else:
+            print("New story found! Checking for duplicates...")
+            if self._check_for_duplicates(title, link, self.reddit.subreddit(sub_info['name'])):
+                new_update = int(time.time()) + 1800  # Check 30 minutes later
+                sources[url]['last_id'] = guid
+                update_entry.update({'update_time': new_update, 'listening': True})
+            else:
+                print("No duplicates found, posting to Reddit...")
+                self._post_to_subreddit(sub_info['name'], title, link,
+                                        current_feed['flair'] if 'flair' in current_feed else None)
+                new_update = int(time.time()) + current_feed['check_interval']
+                sources[url]['last_id'] = guid
+                update_entry.update({'update_time': new_update,
+                                     'update_index': (update_entry['update_index'] + 1) % len(current_feed['urls']),
+                                     'listening': False})
+        return new_update
 
     @staticmethod
-    def check_for_duplicates(title: str, link: str, subreddit: praw.reddit.Subreddit) -> bool:
+    def _check_for_duplicates(title: str, link: str, subreddit: praw.reddit.Subreddit) -> bool:
         """
         Checks if the post is a duplicate by comparing the title and link to posts in the subreddit
-        from the last 24 hours (up to 100 posts).
+        from the last 24 hours (up to 1000 posts).
         Links are checked for exact matches, titles are checked for similarity using a spaCy model.
         The current threshold for similarity is 0.8.
 
@@ -217,26 +274,30 @@ class RedditBot:
         try:
             # Calculate the timestamp for the past 6 hours
             past_timestamp = int((datetime.now() - timedelta(hours=24)).timestamp())
-            for post in subreddit.new(limit=100):
+            for post in subreddit.new(limit=1000):
                 if post.created_utc < past_timestamp:
                     return False
-                if post.url == link:
-                    utils.send_discord_message(f"Duplicate found: reddit.com{post.permalink}")
-                    print(f"Duplicate found: reddit.com{post.permalink}")
+                if utils.remove_query_params(link) == post.url or link == post.url:
+                    utils.send_discord_message(f"Duplicate found: https://www.reddit.com{post.permalink}")
+                    print(f"Duplicate found: https://www.reddit.com{post.permalink}")
                     return True
                 similarity = utils.get_similarity(title, post.title)
                 # Only here for testing, remove later
                 if similarity > 0.8:
                     utils.send_discord_message(
-                        f"Similar title found: {title} and reddit.com{post.permalink} with a similarity of {similarity}")
+                        f"Similar title found with a similarity of {similarity}: \n- {title} \n- {post.title}\n"
+                        f"Source: https://www.reddit.com{post.permalink}")
                     print(f"Similar title found: {title} and {post.title} with a similarity of {similarity}")
                     return True
-            return False
+                elif similarity > 0.75:
+                    utils.send_discord_message(
+                        f"Still posting with a similarity of {similarity}: \n- {title} \n- {post.title}\n"
+                        f"Source: https://www.reddit.com{post.permalink}")
         except Exception as e:
             print(f'Error checking for duplicates: {str(e)}')
             return False
 
-    def post_to_subreddit(self, sub_name: str, title: str, link: str, flair_text: str | None = None) -> None:
+    def _post_to_subreddit(self, sub_name: str, title: str, link: str, flair_text: str | None = None) -> None:
         """
         Posts to the subreddit with the given flair_text if applicable
 
@@ -283,7 +344,7 @@ class RedditBot:
         # Posts to subreddit
         try:
             subreddit = self.reddit.subreddit(sub_name)
-            if self.check_for_duplicates(title, link, subreddit):
+            if self._check_for_duplicates(title, link, subreddit):
                 return
             if flair_text:
                 post_with_flair()
@@ -291,20 +352,3 @@ class RedditBot:
                 post_without_flair()
         except Exception as e:
             print(f'Error posting to {sub_name}: {str(e)}')
-
-    def run(self) -> None:
-        """
-        Main program loop that checks the RSS feeds, updates the db.json file, then sleeps for a specified amount of
-        time.
-        """
-        while True:
-            print(f"\n[{time.strftime('%Y-%m-%d %H:%M')}] Checking RSS feeds...")
-            next_update = self.subreddits_loop()
-            # Update db.json
-            if not self.testing:
-                print("Updating db.json...")
-                utils.update_db(self.db_file, self.db)
-            # Wait until next update
-            t = datetime.fromtimestamp(next_update).strftime('%Y-%m-%d %H:%M')
-            print(f"Next update at {t}")
-            utils.until(next_update)
