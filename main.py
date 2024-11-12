f9f7ed4e73401a18cf077a7169e78f95b2fa61ec
George Wan <gw9999@outlook.com>
2024-11-11 19:27:43 -0500

    Fix no parent directory error

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from rss_script import RedditBot
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--testing", help="Testing mode toggle", action="store_true")
+    os.makedirs(os.path.abspath("db"), exist_ok=True)
     bot = RedditBot(parser.parse_args().testing, os.path.abspath("config.yaml"), os.path.abspath("db/db.json"),
                     os.path.abspath("db/rss_script.log"))
     bot.run()
